e106199c9187473dacca79972f1912695aee58c7
steve-erick <erickcastro.esc@gmail.com>
2025-07-07 22:25:25 -0300

    third-commit

--- frontend/saveit/src/main.server.ts
+++ frontend/saveit/src/main.server.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/components/app';
+import { App } from './app/app';
 import { config } from './app/app.config.server';
 
 const bootstrap = () => bootstrapApplication(App, config);
