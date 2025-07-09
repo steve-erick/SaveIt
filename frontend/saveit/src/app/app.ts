e6e66eafc93a6df857b9f136fbd0339d392b52a7
steve-erick <erickcastro.esc@gmail.com>
2025-07-08 22:49:03 -0300

    fourth-commit

--- frontend/saveit/src/app/app.ts
+++ frontend/saveit/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Navbar } from './components/navbar/navbar';
+import { Sidebar } from './components/sidebar/sidebar';
+import { Panel } from './components/panel/panel';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar,Sidebar,Panel],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
